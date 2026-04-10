dfb465e7162969cb0042df9757efeb568349a3e5
Metry Analytics <admin@metryanalytics.com>
2026-04-10 02:07:41 -0600

    Update i18n.ts

--- i18n.ts
+++ i18n.ts
@@ -1,14 +1,16 @@
-import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
 
-// Can be imported from a shared config
 const locales = ['es', 'en'];
 
-export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
+export default getRequestConfig(async ({requestLocale}) => {
+  let locale = await requestLocale;
+  
+  if (!locale || !locales.includes(locale)) {
+    locale = 'es';
+  }
 
   return {
+    locale,
     messages: (await import(`./messages/${locale}.json`)).default
   };
 });
