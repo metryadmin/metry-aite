f9aa2870154d017d71a464a22b6414861058777b
Metry Analytics <admin@metryanalytics.com>
2026-04-10 07:45:53 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,11 @@
 import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
-  // Las locales soportadas
   locales: ['es', 'en'],
-  
-  // Utilizado cuando no hay match
   defaultLocale: 'es',
   localePrefix: 'as-needed'
 });
 
 export const config = {
-  // Coincide con todas las peticiones (salvo api, _next y static files)
   matcher: ['/((?!api|_next|.*\\..*).*)']
 };
