0a52379a0fa78481f22a7a0743e53c292389766b
Metry Analytics <admin@metryanalytics.com>
2026-04-10 07:46:35 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,13 @@
 import createMiddleware from 'next-intl/middleware';
+import {defineRouting} from 'next-intl/routing';
 
-export default createMiddleware({
+const routing = defineRouting({
   locales: ['es', 'en'],
   defaultLocale: 'es',
   localePrefix: 'as-needed'
 });
+
+export default createMiddleware(routing);
 
 export const config = {
   matcher: ['/((?!api|_next|.*\\..*).*)']
